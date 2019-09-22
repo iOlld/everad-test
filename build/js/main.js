08488b742df36873f7cc3807864f4ca78d31db1b
Zheka-ded <11vitamin11@rambler.ru>
2019-09-22 22:49:52 +0300

    finish 100%

--- build/js/main.js
+++ build/js/main.js
@@ -1,16 +1,1 @@
 // alert(`Hello Ded`);
-// document.querySelector(`.header__input-name`).oninput = function (){
-//     document.querySelector(`.header__input-name-svg`).style.fill = `#a7c93d`;
-// };
-
-// document.querySelector(`.header__input-name`).onfocus = function (){
-//     document.querySelector(`.header__input-name-svg`).style.fill = `#a7c93d`;
-// };
-
-// document.querySelector(`.header__input-name`).onmouseleave = function (){
-//     document.querySelector(`.header__input-name-svg`).style.fill = `#e87845`;
-// };
-
-// document.querySelector(`.header__input-phone`).oninput = function (){
-//     document.querySelector(`.header__input-phone-svg`).style.fill = `#a7c93d`;
-// };
